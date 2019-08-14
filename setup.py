e73fb05b727bd7de60d9ee6ac320c6389218fd64
vandepollt <tom.vandepoll@tno.nl>
2019-08-14 12:56:56 +0200

    Add well_filter and interpret_well_drill executables

--- setup.py
+++ setup.py
@@ -37,6 +37,8 @@ setup(
             "fm_strip_dates = spinningjenny.script.strip_dates:main_entry_point",
             "fm_well_constraints = spinningjenny.script.well_constraints:main_entry_point",
             "fm_add_templates = spinningjenny.script.add_templates:main_entry_point",
+            "fm_well_filter = spinningjenny.script.well_filter:main_entry_point",
+            "fm_interpret_well_drill = spinningjenny.script.interpret_well_drill:main_entry_point",
         ]
     },
 )
