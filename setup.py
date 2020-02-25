a762d0a662ac04d089b65a7c3acf0a2dac0ca98e
Åsmund Birkeland <musiv@equinor.com>
2020-02-25 09:13:54 +0100

    Use singe point of information for entry-points

--- setup.py
+++ setup.py
@@ -1,29 +1,26 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
+import yaml
 
 
 def entry_point_strings():
-    eps = {
-        "drill_planner": "spinningjenny.script.fm_drill_planner:main_entry_point",
-        "npv": "spinningjenny.script.fm_npv:main_entry_point",
-        "rf": "spinningjenny.script.fm_rf:main_entry_point",
-        "schmerge": "spinningjenny.script.fm_schmerge:main_entry_point",
-        "stea": "spinningjenny.script.fm_stea:main_entry_point",
-        "strip_dates": "spinningjenny.script.fm_strip_dates:main_entry_point",
-        "well_constraints": "spinningjenny.script.fm_well_constraints:main_entry_point",
-        "add_templates": "spinningjenny.script.fm_add_templates:main_entry_point",
-        "well_filter": "spinningjenny.script.fm_well_filter:main_entry_point",
-        "interpret_well_drill": "spinningjenny.script.fm_interpret_well_drill:main_entry_point",
-        "extract_summary_data": "spinningjenny.script.fm_extract_summary_data:main_entry_point",
-    }
+    with open("share/entry_points.yml") as fh:
+        eps = yaml.load(fh)
+
     return ["fm_{} = {}".format(k, v) for k, v in eps.items()]
 
 
 setup(
     name="spinningjenny",
     packages=find_packages(include=["spinningjenny*", "share*"]),
-    package_data={"share": ["spinningjenny/forwardmodels/*", "spinningjenny/npv/*"]},
+    package_data={
+        "share": [
+            "entry_points.yml",
+            "spinningjenny/forwardmodels/*",
+            "spinningjenny/npv/*",
+        ]
+    },
     description="Module containing a collection of jobs for ERT",
     author="Equinor ASA",
     url="https://github.com/equinor/spinningjenny",
--- spinningjenny/bin/__init__.py
+++ spinningjenny/bin/__init__.py
@@ -1,3 +1,7 @@
+import yaml
+import pkg_resources
+
+
 def entry_points():
     """
     Dictionary mapping a spinning jenny job name to it's main entry point script function.
@@ -6,16 +10,5 @@ def entry_points():
     will be enforced using a test.
     :return:
     """
-    return {
-        "drill_planner": "spinningjenny.script.fm_drill_planner:main_entry_point",
-        "npv": "spinningjenny.script.fm_npv:main_entry_point",
-        "rf": "spinningjenny.script.fm_rf:main_entry_point",
-        "schmerge": "spinningjenny.script.fm_schmerge:main_entry_point",
-        "stea": "spinningjenny.script.fm_stea:main_entry_point",
-        "strip_dates": "spinningjenny.script.fm_strip_dates:main_entry_point",
-        "well_constraints": "spinningjenny.script.fm_well_constraints:main_entry_point",
-        "add_templates": "spinningjenny.script.fm_add_templates:main_entry_point",
-        "well_filter": "spinningjenny.script.fm_well_filter:main_entry_point",
-        "interpret_well_drill": "spinningjenny.script.fm_interpret_well_drill:main_entry_point",
-        "extract_summary_data": "spinningjenny.script.fm_extract_summary_data:main_entry_point",
-    }
+    with open(pkg_resources.resource_filename("share", "entry_points.yml")) as fh:
+        return yaml.load(fh)
