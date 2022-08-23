45cc3d1fcbdd36f7b6199726870d1e903e419ed5
Sharlon N. Regales <sharlon.regales@tno.nl>
2022-08-23 11:18:32 +0000

    Remove explicit utf-8 python file encoding
    Since python 3+ the default file encoding is utf-8

--- spinningjenny/add_templates/add_tmpl_schema.py
+++ spinningjenny/add_templates/add_tmpl_schema.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 from os import path
 
 from configsuite import MetaKeys as MK
--- spinningjenny/well_constraints/well_config.py
+++ spinningjenny/well_constraints/well_config.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 import configsuite
 from configsuite import MetaKeys as MK
 from configsuite import types
