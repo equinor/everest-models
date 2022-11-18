f2dbe38093a0b5d87a8d5ed4d5c717390d8a594f
Sharlon N. Regales <sharlon.regales@tno.nl>
2022-11-18 18:48:47 +0000

    Refactor drill_date_planner forward model
    - Create "-i/--input" argument schema
    - Expose this schema to users with the "--schema" flag
    - Add linting feature with "--lint" flag
    - Make "-h/--help" more understandable
    - Simplify code (remove tasks module)
    - Better error message

--- src/spinningjenny/jobs/fm_drill_date_planner/cli.py
+++ src/spinningjenny/jobs/fm_drill_date_planner/cli.py
@@ -1,23 +1,50 @@
 #!/usr/bin/env python
+import itertools
 import logging
 
-from spinningjenny.jobs.fm_drill_date_planner import tasks
-from spinningjenny.jobs.fm_drill_date_planner.parser import args_parser
-from spinningjenny.jobs.shared.io_utils import write_json_to_file
+from spinningjenny.jobs.fm_drill_date_planner.parser import build_argument_parser
+from spinningjenny.jobs.shared.converters import rescale_value
 
 logger = logging.getLogger(__name__)
 
 
 def main_entry_point(args=None):
+    args_parser = build_argument_parser()
     options = args_parser.parse_args(args)
+    parser_options_conflicts = []
+    well_dict = options.input.to_dict()
 
-    output = tasks.drill_date_planner(
-        options.input, options.optimizer, options.bounds, options.max_days
+    if options.bounds[1] < options.bounds[0]:
+        parser_options_conflicts.append(
+            f"Invalid bounds: lower bound greater than upper, {options.bounds}"
+        )
+
+    wells, other = itertools.tee(
+        (well_dict.pop(name, name), value) for name, value in options.optimizer.items()
     )
+    if bad_controls := tuple(name for name, _ in other if isinstance(name, str)):
+        parser_options_conflicts.append(
+            "Missing well in controls:\n\t" + ", ".join(bad_controls)
+        )
 
-    if options.output:
-        logger.info("Writing results to {}".format(options.output))
-        write_json_to_file(output, options.output)
+    if well_dict:
+        parser_options_conflicts.append(
+            "Drill time missing for well(s):\n\t" + ", ".join(well_dict)
+        )
+
+    if parser_options_conflicts:
+        args_parser.error("\n".join(parser_options_conflicts))
+
+    if options.lint:
+        args_parser.exit()
+
+    for well, value in wells:
+        well.drill_time += rescale_value(
+            value, options.bounds[0], options.bounds[1], 0, options.max_days
+        )
+
+    logger.info(f"Writing results to {options.output}")
+    options.input.json_dump(options.output)
 
 
 if __name__ == "__main__":
--- tests/jobs/drill_date_planner/test_drill_dates_planner_cli.py
+++ tests/jobs/drill_date_planner/test_drill_dates_planner_cli.py
@@ -1,20 +1,120 @@
+import json
+import pathlib
+import sys
+from typing import Dict, NamedTuple, Tuple
+
+import pytest
 from sub_testdata import DRILL_DATE_PLANNER as TEST_DATA
 
-from spinningjenny.jobs.fm_drill_date_planner.cli import main_entry_point
-from spinningjenny.jobs.shared.io_utils import load_yaml
+from spinningjenny.jobs.fm_drill_date_planner import cli
+from spinningjenny.jobs.shared.models import WellListModel
 
 
-def test_drill_date_planner_main_entry_point(copy_testdata_tmpdir):
-    copy_testdata_tmpdir(TEST_DATA)
-
-    arguments = (
+@pytest.fixture(scope="module")
+def drill_date_planner_args():
+    return (
         "--input wells.json -opt controls.json --bounds 0.0 1.0"
         " --max-days 300 -o output.json"
     ).split()
 
-    main_entry_point(arguments)
 
-    test_output = load_yaml("output.json")
-    expected_output = load_yaml("expected_result.json")
+def missing_controls(*args, **kwargs):
+    with open("controls.json") as fp:
+        controls = json.load(fp)
+    del controls["WELL2"]
+    del controls["WELL4"]
+    return controls
 
-    assert test_output == expected_output
+
+def missing_well(*args, **kwargs):
+    wells = WellListModel.parse_file("wells.json")
+    wells.__root__ = (well for well in wells if int(well.name[-1]) % 2)
+    return wells
+
+
+def test_drill_date_planner_main_entry_point(
+    drill_date_planner_args, copy_testdata_tmpdir
+):
+    copy_testdata_tmpdir(TEST_DATA)
+    cli.main_entry_point(drill_date_planner_args)
+
+    assert (
+        pathlib.Path("output.json").read_bytes()
+        == pathlib.Path("expected_result.json").read_bytes()
+    )
+
+
+def test_drill_date_planner_lint(drill_date_planner_args, copy_testdata_tmpdir):
+    copy_testdata_tmpdir(TEST_DATA)
+    with pytest.raises(SystemExit) as e:
+        cli.main_entry_point([*drill_date_planner_args, "--lint"])
+
+    assert e.value.code == 0
+    assert not pathlib.Path("output.json").exists()
+
+
+class Options(NamedTuple):
+    input: WellListModel
+    optimizer: Dict[str, float]
+    bounds: Tuple[float, float] = (0.1, 1.0)
+    max_days: int = 300
+    output: pathlib.Path = pathlib.Path("output.json")
+    lint: bool = False
+
+
+class MockParser:
+    def __init__(self, options: Options):
+        self._options = options
+
+    def parse_args(self, *args, **kwargs):
+        return self._options
+
+    def error(self, message):
+        sys.stderr.write(message)
+        sys.exit(2)
+
+
+def test_drill_date_planner_missing_control(
+    drill_date_planner_args, monkeypatch, capsys, copy_testdata_tmpdir
+):
+    copy_testdata_tmpdir(TEST_DATA)
+    with open("controls.json") as fp:
+        controls = json.load(fp)
+
+    monkeypatch.setattr(
+        cli,
+        "build_argument_parser",
+        lambda: MockParser(Options(input=missing_well(), optimizer=controls)),
+    )
+
+    with pytest.raises(SystemExit) as e:
+        cli.main_entry_point(drill_date_planner_args)
+
+    assert e.value.code == 2
+    _, err = capsys.readouterr()
+    assert "Missing well in controls:\n\tWELL2, WELL4" in err
+    assert not pathlib.Path("output.json").exists()
+
+
+def test_drill_date_planner_missing_well(
+    drill_date_planner_args, monkeypatch, capsys, copy_testdata_tmpdir
+):
+    copy_testdata_tmpdir(TEST_DATA)
+    monkeypatch.setattr(
+        cli,
+        "build_argument_parser",
+        lambda: MockParser(
+            Options(
+                input=WellListModel.parse_file("wells.json"),
+                optimizer=missing_controls(),
+            )
+        ),
+    )
+
+    with pytest.raises(SystemExit) as e:
+        cli.main_entry_point(drill_date_planner_args)
+
+    assert e.value.code == 2
+    _, err = capsys.readouterr()
+    assert "Drill time missing for well(s):\n\tWELL2, WELL4" in err
+    assert not pathlib.Path("output.json").exists()
