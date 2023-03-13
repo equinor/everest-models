cd5d9bd03338af6803e7a5bf8a09f8a672f92ea2
Sharlon N. Regales <sharlon.regales@tno.nl>
2023-03-13 08:20:11 +0000

    Replace pkg_resources due to module deprication

--- src/spinningjenny/everest_hooks.py
+++ src/spinningjenny/everest_hooks.py
@@ -1,12 +1,25 @@
 import logging
+import sys
+from importlib import resources
 
 from everest.plugins import hookimpl
 
-from spinningjenny import scripts
-
 logger = logging.getLogger(__name__)
+
+FORWARD_MODEL_DIR = "forward_models"
+PACKAGE = "spinningjenny"
 
 
 @hookimpl
 def get_forward_models():
-    return scripts.fm_jobs()
+    if sys.version_info.minor >= 9:
+        jobs = resources.files(PACKAGE) / FORWARD_MODEL_DIR
+    else:
+        with resources.path(PACKAGE, FORWARD_MODEL_DIR) as fd:
+            jobs = fd
+
+    return [
+        {"name": (job_name := job.lstrip("fm_")), "path": str(jobs / job_name)}
+        for job in resources.contents(f"{PACKAGE}.jobs")
+        if job.startswith("fm_")
+    ]
--- tests/integration/test_plugin.py
+++ tests/integration/test_plugin.py
@@ -1,7 +1,7 @@
 import itertools
 import sys
 
-from spinningjenny import scripts
+FORWARD_MODEL_DIR = "forward_models"
 
 
 def test_hooks_registered(plugin_manager):
@@ -9,6 +9,24 @@ def test_hooks_registered(plugin_manager):
 
 
 def test_everest_hooks(plugin_manager):
-    jobs = itertools.chain.from_iterable(plugin_manager.hook.get_forward_models())
-    for job in scripts.fm_jobs():
-        assert job in jobs
+    jobs = {
+        "stea": f"{FORWARD_MODEL_DIR}/stea",
+        "drill_planner": f"{FORWARD_MODEL_DIR}/drill_planner",
+        "schmerge": f"{FORWARD_MODEL_DIR}/schmerge",
+        "extract_summary_data": f"{FORWARD_MODEL_DIR}/extract_summary_data",
+        "drill_date_planner": f"{FORWARD_MODEL_DIR}/drill_date_planner",
+        "strip_dates": f"{FORWARD_MODEL_DIR}/strip_dates",
+        "select_wells": f"{FORWARD_MODEL_DIR}/select_wells",
+        "npv": f"{FORWARD_MODEL_DIR}/npv",
+        "well_constraints": f"{FORWARD_MODEL_DIR}/well_constraints",
+        "add_templates": f"{FORWARD_MODEL_DIR}/add_templates",
+        "rf": f"{FORWARD_MODEL_DIR}/rf",
+        "well_filter": f"{FORWARD_MODEL_DIR}/well_filter",
+        "interpret_well_drill": f"{FORWARD_MODEL_DIR}/interpret_well_drill",
+    }
+    assert all(
+        jobs[job["name"]] in job["path"]
+        for job in itertools.chain.from_iterable(
+            plugin_manager.hook.get_forward_models()
+        )
+    )
