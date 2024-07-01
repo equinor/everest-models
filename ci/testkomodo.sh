db29cf9d04096f4506ce9197a7026ade7cd924c7
DanSava <dan.sava42@gmail.com>
2024-07-01 09:35:09 +0300

    Add docs folder when testing with komodo env

--- ci/testkomodo.sh
+++ ci/testkomodo.sh
@@ -1,3 +1,7 @@
+copy_test_files () {
+    cp -r ${CI_SOURCE_ROOT}/tests ${CI_TEST_ROOT}/tests
+    cp -r ${CI_SOURCE_ROOT}/docs ${CI_TEST_ROOT}/docs
+}
 
 install_test_dependencies () {
     pip install .[test]
