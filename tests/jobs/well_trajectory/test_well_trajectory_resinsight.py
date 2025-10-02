25ca45f5841961fdd03167ad14f35b6106413c8a
StephanDeHoop <stephan.dehoop@tno.nl>
2025-10-02 06:52:05 +0000

    Add temp xfail for multilateral tests

--- tests/jobs/well_trajectory/test_well_trajectory_resinsight.py
+++ tests/jobs/well_trajectory/test_well_trajectory_resinsight.py
@@ -101,6 +101,9 @@ def test_well_trajectory_resinsight_main_entry_point_no_mlt(
     _assert_deviation_files_nonempty(Path.cwd() / "wellpaths", expected_dev_files)
 
 
+@pytest.mark.xfail(
+    reason="recent breaking change in rips API for multilateral wells -> remove xfail once this is fixed"
+)
 @pytest.mark.resinsight
 def test_well_trajectory_resinsight_main_entry_point_mlt(
     well_trajectory_arguments, copy_testdata_tmpdir
@@ -134,6 +137,9 @@ def test_well_trajectory_resinsight_main_entry_point_mlt(
     _assert_deviation_files_nonempty(Path.cwd() / "wellpaths", expected_dev_files)
 
 
+@pytest.mark.xfail(
+    reason="recent breaking change in rips API for multilateral wells -> remove xfail once this is fixed"
+)
 @pytest.mark.resinsight
 def test_well_trajectory_resinsight_main_entry_point_mixed(
     well_trajectory_arguments, copy_testdata_tmpdir
